7bb6745c1dc962ce5a35318d10c31ea835bfa588
chloemormin <chloe.mormin@gmail.com>
2022-11-06 16:54:18 +0100

    mise à jour 6 novembre 2022
    MAJ des projets

--- js/imgsuitsouris.js
+++ js/imgsuitsouris.js
@@ -1,18 +1,44 @@
- document.onmousemove = suitsouris;
-        var width = $(window).width();
-        var height = $(window).height();
+document.onmousemove = suitsouris;
+document.onscroll = suiscroll;
 
-        function suitsouris(evenement) {
-            if (navigator.appName == "Microsoft Internet Explorer") {
-                var x = width - event.x + document.body.scrollLeft;
-                var y = event.y + document.body.scrollTop;
-            }
-            else {
-                var x = evenement.pageX;
-                var y = evenement.pageY;
-            }
-            document.getElementById("image_suit_souris").style.left = width / 2 + (x * 0.2 - 1000) + 'px';
-            if (y < 900) {
-                document.getElementById("image_suit_souris").style.top = height / 2 + (y * 0.2) + 'px';
-            }
-        }
+var width = $(window).width();
+var height = $(window).height();
+
+function suitsouris(evenement) {
+    var x = 0;
+    var y = 0;
+
+    if (navigator.appName == "Microsoft Internet Explorer") {
+        x = width - event.x + document.body.scrollLeft;
+        y = event.y + document.body.scrollTop;
+    }
+    else 
+    {
+        x = evenement.pageX;
+        y = evenement.pageY;
+    }
+     
+    document.getElementById("image_suit_souris").style.display = y > 900 ? 'none' : 'block'
+    
+    document.getElementById("image_suit_souris").style.left = width / 2 + (x * 0.2 - 1000) + 'px';
+    if (y < 900) {
+        console.log("move y")
+        document.getElementById("image_suit_souris").style.top = height / 2 + (y * 0.2) + 'px';
+    }
+}
+
+function suiscroll(evenement) {
+    if (evenement.path[1].pageYOffset > 900) {
+        document.getElementById("image_suit_souris").style.display = 'none';
+    } else {
+        document.getElementById("image_suit_souris").style.display = 'block';
+    }
+}
+
+function diseapear(evenement) {
+    if (@media screen and (max-width:1200px)) {
+        document.getElementById("image_suit_souris").style.display = 'none';
+    } else {
+        document.getElementById("image_suit_souris").style.display = 'block';
+    }
+}
