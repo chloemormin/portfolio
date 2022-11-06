a86c1be34a6e381ebf411920ddfc44111e6a6557
chloemormin <chloe.mormin@gmail.com>
2022-11-06 17:52:12 +0100

    Ajouts des RS

--- js/imgsuitsouris.js
+++ js/imgsuitsouris.js
@@ -1,8 +1,15 @@
 document.onmousemove = suitsouris;
 document.onscroll = suiscroll;
 
+    
 var width = $(window).width();
 var height = $(window).height();
+
+if (width < 1200) {
+    document.getElementById("image_suit_souris").style.display = 'none';
+} else {
+    document.getElementById("image_suit_souris").style.display = 'block';
+}
 
 function suitsouris(evenement) {
     var x = 0;
@@ -17,26 +24,15 @@ function suitsouris(evenement) {
         x = evenement.pageX;
         y = evenement.pageY;
     }
-     
-    document.getElementById("image_suit_souris").style.display = y > 900 ? 'none' : 'block'
     
     document.getElementById("image_suit_souris").style.left = width / 2 + (x * 0.2 - 1000) + 'px';
     if (y < 900) {
-        console.log("move y")
         document.getElementById("image_suit_souris").style.top = height / 2 + (y * 0.2) + 'px';
     }
 }
 
 function suiscroll(evenement) {
     if (evenement.path[1].pageYOffset > 900) {
-        document.getElementById("image_suit_souris").style.display = 'none';
-    } else {
-        document.getElementById("image_suit_souris").style.display = 'block';
-    }
-}
-
-function diseapear(evenement) {
-    if (@media screen and (max-width:1200px)) {
         document.getElementById("image_suit_souris").style.display = 'none';
     } else {
         document.getElementById("image_suit_souris").style.display = 'block';
